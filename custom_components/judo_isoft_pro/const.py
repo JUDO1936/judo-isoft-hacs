1b405ef67524cdc4f74fcf2a0629ac63627cf42d
JUDO Wasseraufbereitung GmbH <Keju04@outlook.com>
2026-09-22 12:06:57 +0200

    Delete custom_components/judo_isoft_pro/const.py

--- custom_components/judo_isoft_pro/const.py
+++
@@ -1,9 +0,0 @@
-"""Konstanten für die JUDO i-soft Integration."""
-DOMAIN = "judo_isoft"
-
-CONF_IP_ADDRESS = "ip_address"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-
-DEFAULT_USERNAME = "admin"
-DEFAULT_PASSWORD = "Connectivity"
